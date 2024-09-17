5d356210e2398bd1c19da441a75e85663c764be0
Bryan112802 <sorianob@mail.gvsu.edu>
2024-09-17 10:42:31 -0400

    Home page update

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,8 +3,16 @@ import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <div className="flex items-center justify-center flex-grow p-6">
-      <h2 className="text-3xl font-bold text-center text-gray-800">
+    <div className="relative flex items-center justify-center min-h-screen text-white bg-gray-800">
+      {/* Add animated lines */}
+      <div className="lines">
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+
+      {/* Welcome message */}
+      <h2 className="z-10 text-4xl font-bold text-center">
         Welcome to Bryan's Parking Pass Generator
       </h2>
     </div>
